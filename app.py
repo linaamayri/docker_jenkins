b8e9504af867f59728c32147816ee219f459019a
KHEfreiIntervenant <71932335+KHEfreiIntervenant@users.noreply.github.com>
2020-11-09 09:32:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	redis_client = StrictRedis(host='redis', port=6379)
+	redis_client = StrictRedis(host='172.17.0.3', port=6379)
 	app.run(host='0.0.0.0')
 	
 	

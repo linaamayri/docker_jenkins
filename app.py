9fa3658ceadc00b28d439882a60b941af9dd0332
KHEfreiIntervenant <71932335+KHEfreiIntervenant@users.noreply.github.com>
2020-11-08 23:48:12 -0800

    Added new exercise files

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, render_template
+from redis import Redis, RedisError, StrictRedis
+import json
+
+app = Flask(__name__)
+
+def add_user(uid, fname, lname):
+	user = {'first_name': fname,
+			'last_name': lname,
+			'credit': 0
+			}
+	status = "success"
+	try:
+		redis_client.set(uid, json.dumps(user))
+	except RedisError:
+		status = "fail"
+	
+	return status
+	
+	
+def add_credit(uid, credit):
+	user = ''
+	status = "success"
+	try:
+		user = redis_client.get(uid)
+	except RedisError:
+		status = "fail"
+	if status is not 'fail' and user is not '':
+		user = json.loads(user)
+		user['credit'] = float(user['credit']) + float(credit)
+		try:
+			redis_client.set(uid, json.dumps(user))
+		except RedisError:
+			status = "fail"
+	else:
+		status = 'fail'
+	return status
+	
+def view_user(uid):
+	user = ''
+	status = ""
+	try:
+		user = redis_client.get(uid)
+	except RedisError:
+		status = "fail"
+		
+	if status is not 'fail' and user is not '':
+		user = json.loads(user)
+		status = "first name: {}, last name: {}, credit: {}".format(
+															user['first_name'],
+															user['last_name'],
+															user['credit']
+														)
+	return status
+	
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		details = request.form
+		if details['form_type'] == 'add_user':
+			return add_user(details['uid'], details['fname'], details['lname'])
+			
+		elif details['form_type'] == 'add_credit':
+			return add_credit(details['uid'], details['credit'])
+			
+		elif details['form_type'] == 'view_user':
+			return view_user(details['uid'])
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	redis_client = StrictRedis(host='redis', port=6379)
+	app.run(host='0.0.0.0')
+	
+	
+	
+	
+	
---
+++ test_app.py
@@ -0,0 +1,68 @@
+import unittest
+import os
+import requests
+
+class FlaskTests(unittest.TestCase):
+
+	def setUp(self):
+		os.environ['NO_PROXY'] = '0.0.0.0'
+		self.user = {
+			'uid': 314,
+			'fname': "Joerge",
+			"lname": "Carmen",
+			"credit": 30
+		}
+		pass
+		
+	def tearDown(self):
+		pass
+	
+	
+	def test_a_index(self):
+		responce = requests.get('http://localhost:5000')
+		self.assertEqual(responce.status_code, 200)
+		
+	def test_b_add_user(self):
+		
+		params = {
+			'uid': self.user['uid'],
+			'fname': self.user['fname'],
+			"lname": self.user['lname'],
+			"form_type": "add_user"
+		}
+		responce = requests.post('http://localhost:5000', data=params)
+		self.assertEqual(responce.status_code, 200)
+		self.assertEqual(responce.content, 'success'.encode())
+		
+	def test_c_add_credit(self):
+		
+		params = {
+			'uid': self.user['uid'],
+			'credit': self.user['credit'],
+			"form_type": "add_credit"
+		}
+		responce = requests.post('http://localhost:5000', data=params)
+		self.assertEqual(responce.status_code, 200)
+		self.assertEqual(responce.content, 'success'.encode())
+	
+	def test_d_view_user(self):
+		params = {
+			'uid': self.user['uid'],
+			"form_type": "view_user"
+		}
+		expected = "first name: {}, last name: {}, credit: {}".format(
+																self.user['fname'],
+																self.user['lname'],
+																float(self.user['credit'])
+																)
+		responce = requests.post('http://localhost:5000', data=params)
+		self.assertEqual(responce.status_code, 200)
+		self.assertEqual(responce.content, expected.encode())
+		
+		
+if __name__ == '__main__':
+	unittest.main()		
+
+
+
+

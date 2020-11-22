6899c62cac4db398df7eb44c5061295a5a0c989a
KHEfreiIntervenant <71932335+KHEfreiIntervenant@users.noreply.github.com>
2020-11-22 07:52:20 -0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -20,7 +20,7 @@ class FlaskTests(unittest.TestCase):
 	
 	def test_a_index(self):
 		responce = requests.get('http://localhost:5000')
-		self.assertEqual(responce.status_code, 200)
+		self.assertEqual(responce.status_code, 205)
 		
 	def test_b_add_user(self):
 		
